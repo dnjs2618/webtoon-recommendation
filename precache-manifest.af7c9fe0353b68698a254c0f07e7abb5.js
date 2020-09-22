30b3706cc11278faff77804096f33bbc57df62ad
Sang won <dnjs2618@naver.com>
2020-09-22 11:58:43 +0900

    Updates

---
+++ precache-manifest.af7c9fe0353b68698a254c0f07e7abb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94e754d43aad9126a83cf7d6ce1c61b9",
+    "url": "/webtoon-recommendation/index.html"
+  },
+  {
+    "revision": "82288e6083a0eadffd54",
+    "url": "/webtoon-recommendation/static/css/main.16d5dc1a.chunk.css"
+  },
+  {
+    "revision": "b183b6819bea0762ff26",
+    "url": "/webtoon-recommendation/static/js/2.832b076a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/webtoon-recommendation/static/js/2.832b076a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82288e6083a0eadffd54",
+    "url": "/webtoon-recommendation/static/js/main.0b6366e2.chunk.js"
+  },
+  {
+    "revision": "6b9451c7c92b19dc4478",
+    "url": "/webtoon-recommendation/static/js/runtime-main.c6edc734.js"
+  }
+]);
